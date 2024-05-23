1062f38f6a93a4fddb7c2f9ad87cdd4b645c2a08
Kroja-Nk <166620425+rizkyaqil04@users.noreply.github.com>
2024-05-23 22:24:28 +0700

    asd

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,30 +1,31 @@
 <x-app-layout>
-    <x-slot name="title">
-        Admin Dashboard
-    </x-slot>
+   <x-slot name="title">
+      Admin Dashboard
+   </x-slot>
 
-    <section class="flex">
+   <section class="flex">
 
-        <!-- Sidebar -->
-        @include('layouts.sidebar')
+      <!-- Sidebar -->
+      @include('layouts.sidebar')
 
-        <!-- Main Content -->
-        <main class="flex-1 p-4 text-white">
-            <div class="container mx-auto">
-                <h2 class="text-2xl font-bold mb-4">Selamat Datang di Dashboard Admin</h2>
-                <div class="grid grid-cols-2 gap-4 mb-8">
-                    <!-- Card untuk jumlah produk -->
-                    <div class="bg-gray-800 p-4 rounded-lg shadow-md">
-                        <h3 class="text-lg font-semibold">Jumlah Produk</h3>
-                        <p class="text-2xl">{{ $productCount }}</p>
-                    </div>
-                    <!-- Card untuk jumlah pengguna -->
-                    <div class="bg-gray-800 p-4 rounded-lg shadow-md">
-                        <h3 class="text-lg font-semibold">Jumlah Pengguna</h3>
-                        <p class="text-2xl">{{ $userCount }}</p>
-                    </div>
-                </div>
+      <!-- Main Content -->
+      <main class="flex-1 p-4 text-white">
+         <div class="container mx-auto">
+            <h2 class="text-2xl font-bold mb-4">Selamat Datang di Dashboard Admin</h2>
+            <div class="grid grid-cols-2 gap-4 mb-8">
+               <!-- Card untuk jumlah produk -->
+               <div class="bg-gray-800 p-4 rounded-lg shadow-md">
+                  <h3 class="text-lg font-semibold">Jumlah Produk</h3>
+                  <p class="text-2xl">{{ $productCount }}</p>
+               </div>
+               <!-- Card untuk jumlah pengguna -->
+               <div class="bg-gray-800 p-4 rounded-lg shadow-md">
+                  <h3 class="text-lg font-semibold">Jumlah Pengguna</h3>
+                  <p class="text-2xl">{{ $userCount }}</p>
+               </div>
+            </div>
 
+<<<<<<< Updated upstream
                 <!-- Card untuk pesanan terbaru -->
                 <div class="flex flex-row justify-between">
                     <h2 class="text-2xl font-bold mb-4">Pesanan Terbaru</h2>
@@ -67,25 +68,38 @@
                                 <label for="nama_produk">Nama produk</label>
                             </div>
                         </div>
+=======
+            <!-- Card untuk pesanan terbaru -->
+            <h2 class="text-2xl font-bold mb-4">Pesanan Terbaru</h2>
+            <form action="{{ route('admin.dashboard') }}" method="GET">
+               <div class="flex flex-row gap-4 flex-shrink-0 mb-4 flex-wrap items-center">
+                  <select name="sort" class="bg-gray-800 text-white p-2 rounded-lg">
+                     <option value="waktu_order" {{ request('sort') == 'waktu_order' ? 'selected' : '' }}>Waktu order
+                     </option>
+                     <option value="nomor_id" {{ request('sort') == 'nomor_id' ? 'selected' : '' }}>Nomor ID</option>
+                     <option value="status" {{ request('sort') == 'status' ? 'selected' : '' }}>Status pesanan</option>
+                     <option value="kuantitas" {{ request('sort') == 'kuantitas' ? 'selected' : '' }}>Kuantitas pesanan
+                     </option>
+                     <option value="harga" {{ request('sort') == 'harga' ? 'selected' : '' }}>Harga total pesanan
+                     </option>
+                     <option value="nama_produk" {{ request('sort') == 'nama_produk' ? 'selected' : '' }}>Nama produk
+                     </option>
+                  </select>
+>>>>>>> Stashed changes
 
-                        <div class="flex flex-col">
-                            <div>
-                                <input type="radio" id="desc" name="direction" value="desc"
-                                    {{ request('direction') == 'desc' ? 'checked' : '' }}>
-                                <label for="desc">Terbesar hingga terkecil</label>
-                            </div>
-                            <div>
-                                <input type="radio" id="asc" name="direction" value="asc"
-                                    {{ request('direction') == 'asc' ? 'checked' : '' }}>
-                                <label for="asc">Terkecil hingga terbesar</label>
-                            </div>
-                        </div>
+                  <select name="direction" class="bg-gray-800 text-white p-2 rounded-lg w-56">
+                     <option value="desc" {{ request('direction') == 'desc' ? 'selected' : '' }}>Terbesar hingga
+                        terkecil</option>
+                     <option value="asc" {{ request('direction') == 'asc' ? 'selected' : '' }}>Terkecil hingga
+                        terbesar</option>
+                  </select>
 
-                        <button type="submit"
-                            class="bg-gray-800 p-4 rounded-lg shadow-md rounded-r-md hover:bg-gray-700 transition-colors duration-300">Urutkan</button>
-                    </div>
-                </form>
+                  <button type="submit"
+                     class="bg-gray-800 py-2 px-3 rounded-lg shadow-md rounded-r-md hover:bg-gray-700 transition-colors duration-300">Urutkan</button>
+               </div>
+            </form>
 
+<<<<<<< Updated upstream
                 <div class="grid grid-cols-2 gap-4">
                     @foreach ($recentOrders as $order)
                         <div class="flex flex-row justify-between bg-gray-800 p-4 rounded-lg shadow-md relative">
@@ -126,8 +140,45 @@
                         </div>
                     @endforeach
                 </div>
+=======
+            <div class="grid grid-cols-2 gap-4">
+               @foreach ($recentOrders as $order)
+                  <div class="bg-gray-800 p-4 rounded-lg shadow-md">
+                     <p>Order ID: {{ $order->nomor_id }}</p>
+                     <p>Total Price: Rp{{ number_format($order->harga, 2) }}</p>
+                     <p>Status: <span
+                           style="color:
+                            @switch($order->status)
+                                @case('pending')
+                                    yellow;
+                                    @break
+                                @case('diproses')
+                                    orange;
+                                    @break
+                                @case('dalam perjalanan')
+                                    blue;
+                                    @break
+                                @case('selesai')
+                                    green;
+                                    @break
+                                @case('canceled')
+                                    red;
+                                    @break
+                            @endswitch
+                        ">{{ $order->status }}</span>
+                     </p>
+                     <p>Order Items:</p>
+                     <ul>
+                        <p>&emsp;&emsp;Product: {{ $order->nama_produk }}</p>
+                        <p>&emsp;&emsp;Color: {{ $order->color }}</p>
+                        <p>&emsp;&emsp;Quantity: {{ $order->kuantitas }}</p>
+                     </ul>
+                  </div>
+               @endforeach
+>>>>>>> Stashed changes
             </div>
-        </main>
+         </div>
+      </main>
 
-    </section>
+   </section>
 </x-app-layout>
